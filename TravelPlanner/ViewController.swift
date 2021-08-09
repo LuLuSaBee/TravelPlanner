c1bafe9618628b27dd0dde839cabdace7fb1d658
LuLuSaBee <lulusabeetw@gmail.com>
2021-08-09 15:31:40 +0800

    Initial Commit

---
+++ TravelPlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelPlanner
+//
+//  Created by Louis SL Liu on 2021/8/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
